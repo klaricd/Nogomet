fe411b220a7009f1312c4ca61ede5ed5b905636d
David <44948149+klaricd@users.noreply.github.com>
2020-05-04 14:57:45 +0200

    Football

---
+++ Assets/Scripts/GameControll.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControll : MonoBehaviour
+{
+    public GameObject BallPrefab;
+    public float BallSpeed;
+    GameObject BallInstance;
+    Vector3 mouseStart;
+    Vector3 mouseEnd;
+
+    float minMove = 15f;
+    float zDepth = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseStart = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseEnd = Input.mousePosition;
+            if(Vector3.Distance(mouseEnd, mouseStart) > minMove)
+            {
+                // shoot ball
+                Vector3 hitPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zDepth);
+                hitPos = Camera.main.ScreenToWorldPoint(hitPos);
+                BallInstance.transform.LookAt(hitPos);
+                BallInstance.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * BallSpeed, ForceMode.Impulse);
+                Invoke("CreateBall", 2f);
+            }
+        }
+    }
+
+    void CreateBall()
+    {
+        BallInstance = Instantiate (BallPrefab, BallPrefab.transform.position, Quaternion.identity) as GameObject;
+    }
+}
